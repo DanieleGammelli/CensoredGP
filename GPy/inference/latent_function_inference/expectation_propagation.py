7c1b687dd5597f93381e1fe196b205d68ba29875
Daniele Gammelli <daniele.gammelli@gmail.com>
2019-10-18 15:51:51 +0200

    Add EP extension for censoring

--- GPy/inference/latent_function_inference/expectation_propagation.py
+++ GPy/inference/latent_function_inference/expectation_propagation.py
@@ -440,6 +440,198 @@ class EP(EPBase, ExactGaussianInference):
         ee._ep_approximation = _ep_approximation
         return ee
 
+class EPCensored(EPBase, ExactGaussianInference):
+    def inference(self, kern, X, likelihood, Y, censoring, mean_function=None, Y_metadata=None, precision=None, K=None):
+        if self.always_reset and not self.loading:
+            self.reset()
+
+        num_data, output_dim = Y.shape
+        assert output_dim == 1, "ep in 1D only (for now!)"
+
+        if mean_function is None:
+            mean_prior = np.zeros(X.shape[0])
+        else:
+            mean_prior = mean_function.f(X).flatten()
+
+        if K is None:
+            K = kern.K(X)
+
+        if self.ep_mode=="nested" and not self.loading:
+            #Force EP at each step of the optimization
+            self._ep_approximation = None
+            post_params, ga_approx, cav_params, log_Z_tilde = self._ep_approximation = self.expectation_propagation(mean_prior, K, Y, censoring, likelihood, Y_metadata)
+        elif self.ep_mode=="alternated" or self.loading:
+            if getattr(self, '_ep_approximation', None) is None:
+                #if we don't yet have the results of runnign EP, run EP and store the computed factors in self._ep_approximation
+                post_params, ga_approx, cav_params, log_Z_tilde = self._ep_approximation = self.expectation_propagation(mean_prior, K, Y, censoring, likelihood, Y_metadata)
+            else:
+                #if we've already run EP, just use the existing approximation stored in self._ep_approximation
+                post_params, ga_approx, cav_params, log_Z_tilde = self._ep_approximation
+        else:
+            raise ValueError("ep_mode value not valid")
+
+        self.loading = False
+
+        return self._inference(Y, mean_prior, K, ga_approx, cav_params, likelihood, Y_metadata=Y_metadata,  Z_tilde=log_Z_tilde)
+
+    def expectation_propagation(self, mean_prior, K, Y, censoring, likelihood, Y_metadata):
+
+        num_data, data_dim = Y.shape
+        assert data_dim == 1, "This EP methods only works for 1D outputs"
+
+        # Makes computing the sign quicker if we work with numpy arrays rather
+        # than ObsArrays
+        Y = Y.values.copy()
+
+        #Initial values - Marginal moments, cavity params, gaussian approximation params and posterior params
+        marg_moments = marginalMoments(num_data)
+        cav_params = cavityParams(num_data)
+        ga_approx, post_params = self._init_approximations(mean_prior, K, num_data)
+
+        #Approximation
+        stop = False
+        iterations = 0
+        while not stop and (iterations < self.max_iters):
+            self._local_updates(num_data, cav_params, post_params, marg_moments, ga_approx, likelihood, Y, censoring, Y_metadata)
+
+            #(re) compute Sigma and mu using full Cholesky decompy
+            post_params = posteriorParams._recompute(mean_prior, K, ga_approx)
+
+            #monitor convergence
+            if iterations > 0:
+                stop = self._stop_criteria(ga_approx)
+            self.ga_approx_old = gaussianApproximation(ga_approx.v.copy(), ga_approx.tau.copy())
+            iterations += 1
+
+        log_Z_tilde = self._log_Z_tilde(marg_moments, ga_approx, cav_params)
+
+        return (post_params, ga_approx, cav_params, log_Z_tilde)
+
+    def _init_approximations(self, mean_prior, K, num_data):
+        #initial values - Gaussian factors
+        #Initial values - Posterior distribution parameters: q(f|X,Y) = N(f|mu,Sigma)
+        if self.ga_approx_old is None:
+            v_tilde, tau_tilde = np.zeros((2, num_data))
+            ga_approx = gaussianApproximation(v_tilde, tau_tilde)
+            Sigma = K.copy()
+            diag.add(Sigma, 1e-7)
+            mu = mean_prior
+            post_params = posteriorParams(mu, Sigma)
+        else:
+            assert self.ga_approx_old.v.size == num_data, "data size mis-match: did you change the data? try resetting!"
+            ga_approx = gaussianApproximation(self.ga_approx_old.v, self.ga_approx_old.tau)
+            post_params = posteriorParams._recompute(mean_prior, K, ga_approx)
+            diag.add(post_params.Sigma, 1e-7)
+            # TODO: Check the log-marginal under both conditions and choose the best one
+        return (ga_approx, post_params)
+
+    def _local_updates(self, num_data, cav_params, post_params, marg_moments, ga_approx, likelihood, Y, censoring, Y_metadata, update_order=None):
+            if update_order is None:
+                update_order = np.random.permutation(num_data)
+            for i in update_order:
+                #Cavity distribution parameters
+                cav_params._update_i(self.eta, ga_approx, post_params, i)
+
+                if Y_metadata is not None:
+                    # Pick out the relavent metadata for Yi
+                    Y_metadata_i = {}
+                    for key in Y_metadata.keys():
+                        Y_metadata_i[key] = Y_metadata[key][i]
+                else:
+                    Y_metadata_i = None
+
+                #Marginal moments
+                marg_moments.Z_hat[i], marg_moments.mu_hat[i], marg_moments.sigma2_hat[i] = likelihood.moments_match_ep(Y[i], cav_params.tau[i], cav_params.v[i], censoring[i], Y_metadata_i=Y_metadata_i)
+
+                #Site parameters update
+                delta_tau, delta_v = ga_approx._update_i(self.eta, self.delta, post_params, marg_moments, i)
+
+                if self.parallel_updates == False:
+                    post_params._update_rank1(delta_tau, delta_v, ga_approx, i)
+
+    def _log_Z_tilde(self, marg_moments, ga_approx, cav_params):
+        # Z_tilde after removing the terms that can lead to infinite terms due to tau_tilde close to zero.
+        # This terms cancel with the coreresponding terms in the marginal loglikelihood
+        return np.sum((
+                np.log(marg_moments.Z_hat)
+                + 0.5*np.log(2*np.pi) + 0.5*np.log(1+ga_approx.tau/cav_params.tau)
+                - 0.5 * ((ga_approx.v)**2 * 1./(cav_params.tau + ga_approx.tau))
+                + 0.5*(cav_params.v * ( ( (ga_approx.tau/cav_params.tau) * cav_params.v - 2.0 * ga_approx.v ) * 1./(cav_params.tau + ga_approx.tau)))
+                ))
+
+    def _ep_marginal(self, mean_prior, K, ga_approx, Z_tilde):
+        post_params = posteriorParams._recompute(mean_prior, K, ga_approx)
+        # Gaussian log marginal excluding terms that can go to infinity due to arbitrarily small tau_tilde.
+        # These terms cancel out with the terms excluded from Z_tilde
+        B_logdet = np.sum(2.0*np.log(np.diag(post_params.L)))
+        S_mean_prior = ga_approx.tau * mean_prior
+        v_centered = ga_approx.v - S_mean_prior
+        log_marginal =  0.5*(
+                        -len(ga_approx.tau) * log_2_pi - B_logdet
+                        + np.sum(v_centered * np.dot(post_params.Sigma, v_centered))
+                        - np.dot(mean_prior, (S_mean_prior - 2*ga_approx.v))
+                        )
+        log_marginal += Z_tilde
+
+        return log_marginal, post_params
+
+    def _inference(self, Y, mean_prior, K, ga_approx, cav_params, likelihood, Z_tilde, Y_metadata=None):
+        log_marginal, post_params = self._ep_marginal(mean_prior, K, ga_approx, Z_tilde)
+
+        tau_tilde_root = np.sqrt(ga_approx.tau)
+        Sroot_tilde_K = tau_tilde_root[:,None] * K
+
+
+        aux_alpha , _ = dpotrs(post_params.L, tau_tilde_root * (np.dot(K, ga_approx.v) +  mean_prior), lower=1)
+        alpha = (ga_approx.v - tau_tilde_root * aux_alpha)[:,None] #(K + Sigma^(\tilde))^(-1) (/mu^(/tilde) -  /mu_p)
+
+        LWi, _ = dtrtrs(post_params.L, np.diag(tau_tilde_root), lower=1)
+        Wi = np.dot(LWi.T,LWi)
+        symmetrify(Wi) #(K + Sigma^(\tilde))^(-1)
+
+        dL_dK = 0.5 * (tdot(alpha) - Wi)
+        dL_dthetaL = likelihood.ep_gradients(Y, cav_params.tau, cav_params.v, np.diag(dL_dK), Y_metadata=Y_metadata, quad_mode='gh')
+        return Posterior(woodbury_inv=Wi, woodbury_vector=alpha, K=K), log_marginal, {'dL_dK':dL_dK, 'dL_dthetaL':dL_dthetaL, 'dL_dm':alpha}
+
+    def to_dict(self):
+        """
+        Convert the object into a json serializable dictionary.
+
+        Note: It uses the private method _save_to_input_dict of the parent.
+
+        :return dict: json serializable dictionary containing the needed information to instantiate the object
+        """
+
+        input_dict = super(EP, self)._save_to_input_dict()
+        input_dict["class"] = "GPy.inference.latent_function_inference.expectation_propagation.EP"
+        if self.ga_approx_old is not  None:
+            input_dict["ga_approx_old"] = self.ga_approx_old.to_dict()
+        if self._ep_approximation is not  None:
+            input_dict["_ep_approximation"] = {}
+            input_dict["_ep_approximation"]["post_params"] = self._ep_approximation[0].to_dict()
+            input_dict["_ep_approximation"]["ga_approx"] = self._ep_approximation[1].to_dict()
+            input_dict["_ep_approximation"]["cav_params"] = self._ep_approximation[2].to_dict()
+            input_dict["_ep_approximation"]["log_Z_tilde"] = self._ep_approximation[3].tolist()
+
+        return input_dict
+
+    @staticmethod
+    def _build_from_input_dict(inference_class, input_dict):
+        ga_approx_old = input_dict.pop('ga_approx_old', None)
+        if ga_approx_old is not None:
+            ga_approx_old = gaussianApproximation.from_dict(ga_approx_old)
+        _ep_approximation_dict = input_dict.pop('_ep_approximation', None)
+        _ep_approximation = []
+        if _ep_approximation is not None:
+            _ep_approximation.append(posteriorParams.from_dict(_ep_approximation_dict["post_params"]))
+            _ep_approximation.append(gaussianApproximation.from_dict(_ep_approximation_dict["ga_approx"]))
+            _ep_approximation.append(cavityParams.from_dict(_ep_approximation_dict["cav_params"]))
+            _ep_approximation.append(np.array(_ep_approximation_dict["log_Z_tilde"]))
+        ee = EP(**input_dict)
+        ee.ga_approx_old = ga_approx_old
+        ee._ep_approximation = _ep_approximation
+        return ee
+
 class EPDTC(EPBase, VarDTC):
     def inference(self, kern, X, Z, likelihood, Y, mean_function=None, Y_metadata=None, Lm=None, dL_dKmm=None, psi0=None, psi1=None, psi2=None):
         if self.always_reset and not self.loading:
